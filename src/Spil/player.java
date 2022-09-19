1e16f92960856dfdd117f9dac108811926c5a257
Soren <s194630@student.dtu.dk>
2022-09-19 15:59:03 +0200

    Public -> Private

--- src/Spil/player.java
+++ src/Spil/player.java
@@ -1,7 +1,7 @@
 package Spil;
 
 public class player {
-    public static void main(String[] args) {
+    private static void main(String[] args) {
         int points = 0;
         String identifier = "";
     }
