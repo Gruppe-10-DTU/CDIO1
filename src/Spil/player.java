55ad7acadd98b0ee5c354ce8c06ae59fb607d7e6
Mpschmidt100 <mpschmidt100@gmail.com>
2022-09-22 11:21:45 +0200

    Added Getters and setters

---
+++ src/Spil/player.java
@@ -0,0 +1,25 @@
+package spil;
+
+public class player {
+
+    private int points = 0;
+    private String identifier = "";
+
+    // getter
+    public int getPoints() {
+        return points;
+    }
+    // setter
+    public void setPoints(int newPoints) {
+        this.points = newPoints;
+    }
+
+    // getter
+    public String getIdentifier() {
+        return identifier;
+    }
+    // setter
+    public void setIdentifier(String newIdentifier) {
+        this.identifier = newIdentifier;
+    }
+}
