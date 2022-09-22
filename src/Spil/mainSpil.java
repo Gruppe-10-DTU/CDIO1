28d1133f7ae5ae9b5897615f2f57b9758543a156
NilasThoegersen <nilas96@gmail.com>
2022-09-22 11:04:48 +0200

    Added Die class. Trying to run it

--- src/Spil/mainSpil.java
+++ src/Spil/mainSpil.java
@@ -1,7 +1,14 @@
 package Spil;
 
 public class mainSpil {
-    public static void main(String[] args) {
-
+    /*public static void main(String[] args) {
+        /*
+        Diceholder diceholder = new Diceholder();
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Arrays.toString(diceholder.roll()));
+        }
+         *
+        System.out.println("Test");
     }
+    */
 }
