dd58f1b7ada87c446d9615765e45b9c9fc5cdc0c
sand4570 <sand4570@stud.kea.dk>
2022-09-26 13:47:12 +0200

    implement extra 2 and 4

--- src/Spil/mainGame.java
+++ src/Spil/mainGame.java
@@ -1,3 +1,5 @@
+import Spil.Diceholder;
+
 import java.util.*;
 import java.util.stream.IntStream;
 import java.util.ArrayList;
@@ -6,7 +8,7 @@ import java.util.ArrayList;
 class mainGame {
     public static void main(String[] args) {
 //        List p1P = null, p2P = null;
-        int p1P = 5, p2P = 39, sP1 = IntStream.of(p1P).sum(), sP2 = IntStream.of(p2P).sum(), v1 = 0, winner = 0, wP1 = 0, wP2 = 0, tT = 0;
+        int p1P = 40, p2P = 40, sP1 = IntStream.of(p1P).sum(), sP2 = IntStream.of(p2P).sum(), v1 = 0, winner = 0, wP1 = 0, wP2 = 0, tT = 0;
         String uITrue = "";
         /*
         System.out.println("Player 1 points: " + sP1); //display sum of value in list
@@ -34,6 +36,17 @@ class mainGame {
                     if (uITrue.toLowerCase().equals("roll") || (uITrue.equals("1"))) {
                         //rolldice append to p1P
                         System.out.println("yoyo");
+
+                        Diceholder holder = new Diceholder();
+                        int[] rolls = holder.roll();
+
+                        System.out.println(rolls[0] + ", " +rolls[1]);
+
+                        if (rolls[0] == rolls[1]) {
+                            int[] rolls2 = holder.roll();
+                            System.out.println(rolls2[0] + ", " +rolls2[1]);
+                        }
+
                         v1 = v1 + 1;
                         tT = tT + 1;
                         p1P = p1P + 1;
@@ -54,6 +67,17 @@ class mainGame {
                     uITrue = uI.nextLine();
                     if (uITrue.toLowerCase().equals("roll") || (uITrue.equals("1"))) {
                         //rolldice append to p2P
+
+                        Diceholder holder = new Diceholder();
+                        int[] rolls = holder.roll();
+
+                        System.out.println(rolls[0] + ", " +rolls[1]);
+
+                        if (rolls[0] == rolls[1]) {
+                            int[] rolls2 = holder.roll();
+                            System.out.println(rolls2[0] + ", " +rolls2[1]);
+                        }
+
                         v1 = v1 - 1;
                         System.out.println("BRRRRRRA");
                         turn = true;
@@ -69,14 +93,57 @@ class mainGame {
                 }
             } else if ((sP1 >= 40 && winner <= 0) || (sP2 >= 40 && winner <= 0)) {
                 if (sP1 > 40) {
-                    System.out.println("Player 1 wins!");
-                    winner = winner + 1;
-                    wP1 = wP1 + 1;
+
+                    System.out.println("Please select one of the following actions:");
+                    System.out.println("1. Roll dice");
+                    System.out.println("2. Show points");
+                    System.out.println("3. Show rules");
+                    uITrue = uI.nextLine();
+                    if (uITrue.toLowerCase().equals("roll") || (uITrue.equals("1"))) {
+
+                        Diceholder holder = new Diceholder();
+                        int[] rolls = holder.roll();
+
+                        System.out.println(rolls[0] + ", " +rolls[1]);
+
+                        if (rolls[0] == rolls[1]) {
+                            System.out.println("Player 1 wins!");
+                            winner = winner + 1;
+                            wP1 = wP1 + 1;
+                        }
+                    } else if (uITrue.toLowerCase().equals("show points") || (uITrue.equals("2"))) {
+                        //show points
+                        System.out.println("Player 1 points: " + sP1);
+                    } else if (uITrue.toLowerCase().equals("show rules") || (uITrue.equals("3"))) {
+                        //show rules
+                        System.out.println("We rulez");
+                    }
 
                 } else {
-                    System.out.println("Player 2 wins!");
-                    winner = winner + 1;
-                    wP2 = wP2 + 1;
+                    System.out.println("Please select one of the following actions:");
+                    System.out.println("1. Roll dice");
+                    System.out.println("2. Show points");
+                    System.out.println("3. Show rules");
+                    uITrue = uI.nextLine();
+                    if (uITrue.toLowerCase().equals("roll") || (uITrue.equals("1"))) {
+
+                        Diceholder holder = new Diceholder();
+                        int[] rolls = holder.roll();
+
+                        System.out.println(rolls[0] + ", " +rolls[1]);
+
+                        if (rolls[0] == rolls[1]) {
+                            System.out.println("Player 2 wins!");
+                            winner = winner + 1;
+                            wP2 = wP2 + 1;
+                        }
+                    } else if (uITrue.toLowerCase().equals("show points") || (uITrue.equals("2"))) {
+                        //show points
+                        System.out.println("Player 1 points: " + sP1);
+                    } else if (uITrue.toLowerCase().equals("show rules") || (uITrue.equals("3"))) {
+                        //show rules
+                        System.out.println("We rulez");
+                    }
 
                 }
             } else if (winner > 0) {
