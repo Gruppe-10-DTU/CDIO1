991f5cce23d9b5fb301aba8a006b4ccb559d1ea1
NilasThoegersen <nilas96@gmail.com>
2022-09-26 16:01:12 +0200

    Added isEqual back since it's used for testing.

--- src/Spil/Diceholder.java
+++ src/Spil/Diceholder.java
@@ -45,6 +45,13 @@ public class Diceholder{
         }
         return totalValue;
     }
+    //See if the n die are equal
+    public boolean isEqual(){
+        if(Arrays.stream(rolls).distinct().count() == 1){
+            return true;
+        }
+        return false;
+    }
 
     @Override
     public String toString() {
